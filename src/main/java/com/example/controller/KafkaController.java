13ef36d0cba9040a36521c465f89d2dda8f1ab83
waghsmita <smitawagh1@gmail.com>
2021-10-13 13:25:31 +0530

    fd

---
+++ src/main/java/com/example/controller/KafkaController.java
@@ -0,0 +1,24 @@
+
+package com.example.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.producer.Sender;
+
+
+@RestController
+
+@RequestMapping(value ="/user")
+public class KafkaController {
+	@Autowired
+    private final Sender kafkaProducer =new Sender();
+
+    @PostMapping("/{name}")
+    public void sendData(@RequestParam("name") String message) {
+    	kafkaProducer.send(message);
+        System.out.println("sender"+message);
+    }
+}
